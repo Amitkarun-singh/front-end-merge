433c7c4c4deb572b236c5e654796d9a95c7f4cbf
Ajay <ajaysk3433@gmail.com>
2026-02-28 21:41:57 +0530

    Update server configuration in app.config.js to point to the production server

--- app.config.js
+++ app.config.js
@@ -1,2 +1,4 @@
-export const config = { server: "http://localhost:3000" };
+export const config = {
+  server: "https://lightsalmon-duck-483366.hostingersite.com",
+};
 // export const config = { server: "stude.schools2ai.com" };
