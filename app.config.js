622110f88f185c12957cf5fe52b6119375476122
Ajay <ajaysk3433@gmail.com>
2026-03-18 21:48:50 +0530

    Update server configuration in app.config.js and add authorization header in AIPracticePage for API requests.

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
-// export const config = {
-//   server: "http://sandybrown-manatee-216868.hostingersite.com",
-// };
 export const config = {
-  server: "http://localhost:3000",
+  server: "http://sandybrown-manatee-216868.hostingersite.com",
 };
+// export const config = {
+//   server: "http://localhost:3000",
+// };
