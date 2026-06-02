21ea17325546759d191fb5a6ac87c07c3b6d9790
Ajay <Ajaysk3433@gmail.com>
2026-06-02 04:15:37 +0000

    update

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export const config = {
   // Production server — used for auth, profile, etc.
 
-  //server: "https://app-api.schools2ai.com",
+  //server: "https://app-server.schools2ai.com",
   server: "http://localhost:3000",
 
 };
